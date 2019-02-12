932294d53c859442f9a47459cbc9abc7f8e9268e
Keichi Takahashi <keichi.t@me.com>
2019-02-12 19:25:54 +0900

    MBC1 RAM

--- src/catridge.rs
+++ src/catridge.rs
@@ -7,8 +7,11 @@ pub struct Catridge {
     rom: Vec<u8>,
     ram: Vec<u8>,
     mbc_type: u8,
-    rom_bank: u8,
-    ram_bank: u8,
+    ram_enable: bool,
+    bank_no_upper: u8,
+    bank_no_lower: u8,
+    num_rom_banks: u8,
+    mode: bool,
 }
 
 impl Catridge {
@@ -21,6 +24,7 @@ impl Catridge {
             0 => 32 * 1024,
             n => 32 * 1024 << (n as usize),
         };
+        let num_rom_banks = 2 << rom[0x0148];
         let ram_size: usize = match rom[0x0149] {
             0 => 0,
             1 => 2 * 1024,
@@ -45,16 +49,42 @@ impl Catridge {
             panic!("ROM header is corrupted");
         }
 
-        println!("ROM size {}", rom_size);
-        println!("RAM size {}", ram_size);
-        println!("MBC type {}", mbc_type);
+        debug!("ROM size {}", rom_size);
+        debug!("RAM size {}", ram_size);
+        debug!("MBC type {}", mbc_type);
 
         Catridge {
             rom: rom,
             ram: vec![0; ram_size],
             mbc_type: mbc_type,
-            rom_bank: 0,
-            ram_bank: 0,
+            ram_enable: false,
+            bank_no_upper: 0,
+            bank_no_lower: 0,
+            num_rom_banks: num_rom_banks,
+            mode: false,
+        }
+    }
+
+    fn rom_bank_no(&self) -> u8 {
+        let bank_no = if self.mode {
+            self.bank_no_lower
+        } else {
+            self.bank_no_upper << 5 | self.bank_no_lower
+        };
+
+        let bank_no = match bank_no {
+            0 | 0x20 | 0x40 | 0x60 => bank_no + 1,
+            _ => bank_no,
+        };
+
+        bank_no & (self.num_rom_banks - 1)
+    }
+
+    fn ram_bank_no(&self) -> u8 {
+        if self.mode {
+            self.bank_no_upper
+        } else {
+            0
         }
     }
 }
@@ -62,34 +92,36 @@ impl Catridge {
 impl IODevice for Catridge {
     fn write(&mut self, addr: u16, val: u8) {
         match addr {
-            0x0000...0x1fff => {
-                // TODO RAM enable/disable
+            // RAM enable
+            0x0000...0x1fff => self.ram_enable = val & 0x0f > 0,
+            // ROM bank number (lower 5 bits)
+            0x2000...0x3fff => self.bank_no_lower = val & 0x1f,
+            // RAM bank number or ROM bank number (upper 2 bits)
+            0x4000...0x5fff => self.bank_no_upper = val & 0x03,
+            // ROM/RAM mode select
+            0x6000...0x7fff => self.mode = val & 0x01 > 0,
+            // RAM bank 00-03
+            0xa000...0xbfff => {
+                let offset = (8 * 1024) * self.ram_bank_no() as usize;
+                self.ram[(addr & 0x1fff) as usize + offset] = val
             }
-            0x2000...0x3fff => {
-                self.rom_bank = (self.rom_bank & 0xe0) | (val & 0x1f);
-            }
-            0x4000...0x5fff => {
-                self.rom_bank = (self.rom_bank & 0x9f) | (val & 0x60);
-            }
-            0xa000...0xbfff => println!("dame"),
             _ => panic!("Invalid address: 0x{:04x}", addr),
         }
     }
 
     fn read(&self, addr: u16) -> u8 {
         match addr {
+            // ROM bank 00
             0x0000...0x3fff => self.rom[addr as usize],
+            // ROM bank 01-7f
             0x4000...0x7fff => {
-                let bank = match self.rom_bank {
-                    0 | 0x20 | 0x40 | 0x60 => self.rom_bank + 1,
-                    _ => self.rom_bank,
-                };
-                let offset = (16 * 1024) * bank as usize;
+                let offset = (16 * 1024) * self.rom_bank_no() as usize;
                 self.rom[(addr & 0x3fff) as usize + offset]
             }
+            // RAM bank 00-03
             0xa000...0xbfff => {
-                println!("dame");
-                0xff
+                let offset = (8 * 1024) * self.ram_bank_no() as usize;
+                self.ram[(addr & 0x1fff) as usize + offset]
             }
             _ => panic!("Invalid address: 0x{:04x}", addr),
         }
